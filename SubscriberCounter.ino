b61e03dd81ae80a5c23ff4e14cf87dad44833303
M1dn1ght N1nj4 <40809511+M1dn1ghtN1nj4@users.noreply.github.com>
2019-09-13 01:47:33 -0400

    Cleanup and Alignment
    * Added channel name on 1st row
    * Added subscriber count to 3rd row
    * Added total views to 4th row
    * Added some info on personalization
    * Removed an unneeded section
    * Fixed a few display bugs
    * Fixed some whitespaces and alignment

--- SubscriberCounter.ino
+++ SubscriberCounter.ino
@@ -4,87 +4,75 @@
 #include <LiquidCrystal_I2C.h>
 #include <ArduinoJson.h>
 
-const char* ssid     = "SSID-HERE";       // SSID of local network
-const char* password = "PASSWORD-HERE";   // Password on network
-String API_KEY = "API-KEY-HERE";            // YouTube Data API v3 key generated here: https://console.developers.google.com
-String CHANNEL_ID = "CHANNEL-ID-HERE";   // YouTube channel id
+const char* ssid     = "SSID-HERE";      // SSID of local network
+const char* password = "PASSWORD-HERE";  // Password for network
+String API_KEY = "API-KEY-HERE";         // YouTube Data API v3 key generated here: https://console.developers.google.com
+String CHANNEL_ID = "CHANNEL-ID-HERE";   // YouTube channel ID
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
-
-
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address 
 
 WiFiClientSecure client;
 YoutubeApi api(API_KEY, client);
 
-unsigned long api_mtbs = 5000; //mean time between api requests
-unsigned long api_lasttime;   //last time api request has been done
+unsigned long api_mtbs = 10000; //mean time between api requests
+unsigned long api_lasttime;     //last time api request has been done
 long subs = 0;
 
 void setup() {
-  Serial.begin(9600);
-  // Set WiFi to station mode and disconnect from an AP if it was Previously
-  // connected
-  WiFi.hostname("ESP8266SubscriberCounter"); //This changes the hostname of the ESP8266 to display neatly on the network router.
+  lcd.begin(20,4);
+  lcd.clear();
+  //Serial.begin(9600);  //Uncomment this line if you want to view the serial data
+  int cursorPosition=0;
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.hostname("ESP8266SubscriberCounter");
   client.setInsecure();
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
 
   // Attempt to connect to Wifi network:
-  Serial.print("Connecting Wifi: ");
-  Serial.println(ssid);
+  lcd.print("Connecting");
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
 
-  //Set up the LCD
-  lcd.begin(20,4); 
-  lcd.setCursor(0,0);
-  delay(2000);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    lcd.setCursor(cursorPosition,1);
+    lcd.print(".");
+    cursorPosition++;
+  }
+
   lcd.clear();
+  lcd.print("Connected!");
+  delay(1000);
 }
 
-
-
-
-
-
 void loop() {
-   
+
 if (millis() - api_lasttime > api_mtbs)  {
     if(api.getChannelStatistics(CHANNEL_ID))
     {
-      //Serial.println("---------Stats---------");
-      //Serial.print("Subscriber Count: ");
-      //Serial.println(api.channelStats.subscriberCount);
-      //Serial.print("View Count: ");
-      //Serial.println(api.channelStats.viewCount);
-      //Serial.print("Comment Count: ");
-      //Serial.println(api.channelStats.commentCount);
-      //Serial.print("Video Count: ");
-      //Serial.println(api.channelStats.videoCount);
-      // Probably not needed :)
-      //Serial.print("hiddenSubscriberCount: ");
-      //Serial.println(api.channelStats.hiddenSubscriberCount);
-      //Serial.println("------------------------");
-      //SaveStats();
+      // Modify the "setCursor" positions to frame the displayed items
+      // Format (for a 2004 LCD, 20x4):  setCursor(column, row)
+      // (0,0) is the top left position
+      // (19,3) is the bottom right position
+      
       lcd.clear();
+      lcd.setCursor(3,0);
+      lcd.print("YOUR-CHANNEL-NAME-HERE");
+
+      lcd.setCursor(1,2);
+      lcd.print("Subscribers:");
+
+      lcd.setCursor(15,2);
       lcd.print(api.channelStats.subscriberCount);
 
-      
+      lcd.setCursor(0,3);
+      lcd.print("Total views:");
+      lcd.setCursor(14,3);
+      lcd.print(api.channelStats.viewCount);
+
     }
     api_lasttime = millis();
   }
 }
-
-void SaveStats() {
-  lcd.print(String(api.channelStats.subscriberCount));
-  lcd.print(api.channelStats.subscriberCount);
-  }
